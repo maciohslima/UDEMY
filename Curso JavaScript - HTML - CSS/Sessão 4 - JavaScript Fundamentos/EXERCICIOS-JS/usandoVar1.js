1b8cab82aa7ddaca81c471876e8b398db5491f1a
maciohslima <admindebian@DESKTOP-8088Q81.localdomain>
2022-05-21 16:27:58 -0300

    usandoVar1.js

--- Curso JavaScript - HTML - CSS/Sessão 4 - JavaScript Fundamentos/EXERCICIOS-JS/usandoVar1.js
+++ Curso JavaScript - HTML - CSS/Sessão 4 - JavaScript Fundamentos/EXERCICIOS-JS/usandoVar1.js
@@ -1,10 +1,19 @@
-{{{{var sera = 'Será???'}}}}
+{ { { { var sera = 'Será???' } } } }
 
 console.log(sera);
 
 function escreveSera() {
-    var sera2 = 'Será - 2???';
-    console.log(sera2);
+    // var acrescenta = 0;
+
+    var segundoTermo = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"];
+
+    for (let a = 0; a <= 6; a++) {
+        var sera2 = `${segundoTermo[a]} - Feira`;
+        console.log(sera2);
+        // console.log(segundoTermo);
+    }
+    // var sera2 = `Será??? - ${acrescenta++}`;
+    // console.log(sera2);
     // return sera2;
 }
 
