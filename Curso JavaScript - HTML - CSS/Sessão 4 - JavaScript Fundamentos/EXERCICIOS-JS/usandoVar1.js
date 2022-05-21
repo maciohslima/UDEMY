4f1ef132737c2269c83a69de494a651077683c99
maciohslima <admindebian@DESKTOP-8088Q81.localdomain>
2022-05-21 13:09:40 -0300

    usandoVar1.js

---
+++ Curso JavaScript - HTML - CSS/Sessão 4 - JavaScript Fundamentos/EXERCICIOS-JS/usandoVar1.js
@@ -0,0 +1,12 @@
+{{{{var sera = 'Será???'}}}}
+
+console.log(sera);
+
+function escreveSera() {
+    var sera2 = 'Será - 2???';
+    console.log(sera2);
+    // return sera2;
+}
+
+retornaSera = new escreveSera();
+// console.log(sera2);
