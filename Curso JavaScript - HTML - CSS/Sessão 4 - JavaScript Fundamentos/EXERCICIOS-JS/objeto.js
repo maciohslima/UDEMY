923b6c1774e4b638e13231a670b389fff04205cc
maciolima <macio-helton@outlook.com>
2022-05-21 10:08:37 -0300

    objeto

---
+++ Curso JavaScript - HTML - CSS/Sessão 4 - JavaScript Fundamentos/EXERCICIOS-JS/objeto.js
@@ -0,0 +1,15 @@
+const prod1 = {}
+prod1.nome = 'Celular Motorola Moto Z3 Play';
+prod1.preco = 2999.99;
+prod1['Desconto Legal'] = 0.40; // evitar atributos com espaço
+
+console.log(prod1);
+
+const prod2 = {
+    tipoProduto: 'Camisa Polo',
+    marca: 'Reserva',
+    cor: 'Branco',
+    preco: 49.90
+};
+
+console.log(prod2);
