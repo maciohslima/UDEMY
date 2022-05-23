7f4b500beae70d68a268ad5a63f1a57eb5aa950f
maciohslima <macio-helton@outlook.com>
2022-05-23 08:47:27 -0300

    atribuicao e destructuring

---
+++ Curso JavaScript - HTML - CSS/Sessão 4 - JavaScript Fundamentos/EXERCICIOS-JS/destructuring.js
@@ -0,0 +1,28 @@
+// novo recurso do EC2015
+
+const pessoa = {
+    nome: 'Mácio Lima',
+    idade: 33,
+    endereco: {
+        logradouro: 'Rua José Ramos de Vasconcelos',
+        numero: 1054,
+        complemento: 'Apt. 103'
+    }
+}
+
+const { nome, idade } = pessoa;
+console.log(nome, idade);
+
+const { nome: n, idade: i } = pessoa;
+console.log(n, i);
+
+const { sobrenome, bemHumorado = true } = pessoa;
+console.log(sobrenome, bemHumorado);
+
+//acessar objeto dentro de objeto
+const { endereco: { logradouro, numero, cep } } = pessoa;
+console.log(logradouro, numero, cep);
+
+//erro ao acessar objeto não instanciado
+const { conta: { ag, num } } = pessoa;
+console.log(ag, num);
