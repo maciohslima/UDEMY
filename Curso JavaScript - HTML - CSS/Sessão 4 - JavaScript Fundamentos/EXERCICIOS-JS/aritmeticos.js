431936d52573dd555b5d62276ffe9a8349f84c7a
maciohslima <macio-helton@outlook.com>
2022-05-23 11:06:13 -0300

    aritmeticos.js

---
+++ Curso JavaScript - HTML - CSS/Sessão 4 - JavaScript Fundamentos/EXERCICIOS-JS/aritmeticos.js
@@ -0,0 +1,19 @@
+const [a, b, c, d] = [3, 5, 1, 15]; //Atribuindo valores para operações
+
+const soma = a + b + c + d; //Operação de Soma
+const subtracao = d - b; //Operação de subtração
+const multiplicacao = a * b; //Operação de multiplicação
+const divisao = d / a; //Operação de divisão
+const resto = a % 2; //Operação de módulo ou resto da divisão
+
+//Imprimindo valores na tela
+console.log(`
+    O valor da Soma é: ${soma}
+    O valor da subtração é: ${subtracao}
+    O valor da multiplicação é: ${multiplicacao}
+    O valor da divisão é: ${divisao}
+    O valor do módulo da divisão é: ${resto}
+    .
+    ..
+    ...Fim do programa
+    `);
