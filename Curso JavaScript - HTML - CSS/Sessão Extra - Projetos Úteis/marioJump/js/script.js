7ab647316deed8849b5f3f7a6913a6b58fa2b367
maciohslima <macio-helton@outlook.com>
2022-06-16 15:02:08 -0300

    marioJump - Finalizado

--- Curso JavaScript - HTML - CSS/Sessão Extra - Projetos Úteis/marioJump/js/script.js
+++ Curso JavaScript - HTML - CSS/Sessão Extra - Projetos Úteis/marioJump/js/script.js
@@ -1,25 +1,38 @@
 const pipe = document.querySelector('.pipe');
-const mario = document.querySelector(".mario");
+const mario = document.querySelector('.mario');
+const clouds = document.querySelector('.clouds');
 
 const jump = () => {
     mario.classList.add('jump');
 
     setTimeout(() => {
         mario.classList.remove('jump');
-    }
-        , 500)
+    }, 500)
 }
 
 const loop = setInterval(() => {
+
+    // console.log(loop);
+
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const cloudsPosition = clouds.offsetLeft;
 
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 109) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = 'img/game-over.png';
+        mario.style.width = '80px';
+        mario.style.marginLeft = '50px';
+
+        clouds.style.animation = 'none';
+        clouds.style.left = `${cloudsPosition}px`
+
+        clearInterval(loop);
     }
 
 }, 10);
