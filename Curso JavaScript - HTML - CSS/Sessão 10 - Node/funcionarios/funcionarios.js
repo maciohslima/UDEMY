9989ef4d9cac617f3e410dc0dfc96ae2241558b1
maciohslima <macio-helton@outlook.com>
2024-04-02 11:07:17 -0300

    Brazil - Homem - Maior Salário

--- Curso JavaScript - HTML - CSS/Sessão 10 - Node/funcionarios/funcionarios.js
+++ Curso JavaScript - HTML - CSS/Sessão 10 - Node/funcionarios/funcionarios.js
@@ -1,6 +1,9 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json';
 const axios = require('axios');
 
+const homens = f => f.genero === 'M';
+const brasileiros = f => f.pais === 'Brazil';
+
 const chineses = f => f.pais === 'China';
 const mulheres = f => f.genero === 'F';
 const menorSalario = (func, funcAtual) => {
@@ -19,3 +22,14 @@ axios.get(url).then(response => {
     
     console.log(func);
 });
+
+axios.get(url).then(response => {
+    const funcionarios = response.data
+
+    const func = funcionarios
+        .filter(brasileiros)
+        .filter(homens)
+        .filter(menorSalario)
+
+    console.log(func)
+})
