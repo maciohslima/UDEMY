7c5a636b83eef13fde5f9fdff5f792e31deb72ad
maciohslima <macio-helton@outlook.com>
2024-05-03 19:05:00 -0300

    Seção 03

---
+++ Python3/secao_03/aula6.py
@@ -0,0 +1,6 @@
+# conversão de tipos, coerção
+# type convertion, typacasting, coercion
+# É o ato de converter um tipo em outro
+# tipos imutáveis e primitivos
+# str, int, float, bool
+
---
+++ Python3/secao_03/print.py
@@ -0,0 +1,4 @@
+print("Olá Mundo!!!")
+
+nome = input("Qual seu nome: ")
+print("Seja bem vindo {}!".format(nome))
