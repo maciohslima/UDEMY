b22b78452e9c0fc20667da94aa114b14ad1897af
maciohslima <macio-helton@outlook.com>
2024-05-03 21:31:51 -0300

    Seção 03 - aula8.py

--- Python3/secao_03/aula6.py
+++ Python3/secao_03/aula6.py
@@ -3,4 +3,11 @@
 # É o ato de converter um tipo em outro
 # tipos imutáveis e primitivos
 # str, int, float, bool
+print(int('1'), type(int('1')))
+print(float('1') + 1)
 
+a = input('Insira um número: ')
+b = input('Insira um segundo número: ')
+soma = int(a) + int(b)
+
+print("A Soma de {} + {} é igual a: {}".format(a,b,soma))
