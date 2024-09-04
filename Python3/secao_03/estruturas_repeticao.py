99804b2cc9a9ef5406aad407e6872311093fb325
maciohslima <heltonlima@DESKTOP-8088Q81>
2024-09-04 13:15:50 -0300

    Ajuste de Pastas - Python

--- Python3/secao_03/estruturas_repeticao.py
+++ Python3/secao_03/estruturas_repeticao.py
@@ -5,9 +5,17 @@ while (enquanto)
 Executa uma ação enquanto uma condição for verdadeira
 """
 
-condicao = True
+# condicao = True
 
-while condicao:
-    nome = input('Qual o seu nome: ')
-    print(f'Seu nome é {nome}')
-    break
+# while condicao:
+#     nome = input('Qual o seu nome: ')
+#     print(f'Seu nome é {nome}')
+#     break
+
+contador = 0
+
+while contador <= 10:
+    print(contador)
+    contador = contador + 1
+
+print('Fim do código')
