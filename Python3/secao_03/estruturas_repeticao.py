5665d75ea5f202f37bef6eba5e0d8c5f2f7cf1dc
maciolima <macio-helton@outlook.com>
2024-09-20 17:22:15 -0300

    While - Estrutura

--- Python3/secao_03/estruturas_repeticao.py
+++ Python3/secao_03/estruturas_repeticao.py
@@ -12,10 +12,29 @@ Executa uma ação enquanto uma condição for verdadeira
 #     print(f'Seu nome é {nome}')
 #     break
 
+# contador = 0
+
+# while contador <= 10:
+#     print(contador)
+#     contador += 1
+
+# print('Fim do código')
+
 contador = 0
 
-while contador <= 10:
-    print(contador)
+while contador <= 100:
     contador += 1
 
-print('Fim do código')
+    if contador == 6:
+        print('Pula o 6...')
+        continue
+
+    if contador >= 10 and contador <= 27:
+        continue
+
+    print(contador)
+
+    if contador == 40:
+        break
+
+print('Acabou')
