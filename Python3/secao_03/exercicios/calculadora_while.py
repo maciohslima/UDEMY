0650881e4168d12416728ddbb70b33bd6794bff7
maciohslima <macio-helton@outlook.com>
2024-09-26 17:56:56 -0300

    calculadora

---
+++ Python3/secao_03/exercicios/calculadora_while.py
@@ -0,0 +1,50 @@
+"""Calculadora com While"""
+
+while True:
+    numero_1 = input('Digite um número: ')
+    numero_2 = input('Digite outro número: ')
+    operador = input('Digite o operador: [+-/*]')
+
+    numeros_validos = None
+    num_1_float = 0
+    num_2_float = 0
+
+    try:
+        num_1_float = float(numero_1)
+        num_2_float = float(numero_2)
+        numeros_validos = True
+    except:
+        numeros_validos = None
+    
+    if numeros_validos is None:
+        print('Um dos números ou ambos são inválidos...')
+        continue
+
+    operadores_permitidos = '+-/*'
+
+    if operador not in operadores_permitidos:
+        print('O operador é inválido...')
+        continue
+    
+    if len(operador) > 1:
+        print('Digite apenas um operador...')
+        continue
+    
+    print('Realizado sua conta. Confira o resultado abaixo: ')
+
+    if operador == '+':
+        print(f'{num_1_float} + {num_2_float} = ',num_1_float + num_2_float)
+    elif operador == '-':
+        print(f'{num_1_float} - {num_2_float} = ',num_1_float - num_2_float)
+    elif operador == '*':
+        print(f'{num_1_float} * {num_2_float} = ',num_1_float * num_2_float)
+    elif operador == '/':
+        print(f'{num_1_float} / {num_2_float} = ',num_1_float / num_2_float)
+    else:
+        print('Essa frase não deveria aparecer no seu código...')
+
+    ###########
+    sair = input('Quer sair? [s]im: ').lower().startswith('s')
+
+    if sair:
+        break
